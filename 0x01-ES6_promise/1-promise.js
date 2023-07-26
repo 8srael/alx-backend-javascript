17f7ca26bef8455fd2a6b01e0281ecf053c328d4
8srael <kouassiisrael36@gmail.com>
2023-07-26 04:16:45 +0000

    Task 1,  Don't make a promise...if you know you can't keep itwq

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(sucess) {
   return new Promise((resolve, reject) => {
-    if (sucess) {
+    if  (sucess) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(Error('The fake API is not working currently'));
