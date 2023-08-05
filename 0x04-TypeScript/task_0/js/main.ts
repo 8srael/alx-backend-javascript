7bba6478a656eb21d81336701cf4bf3e7b181410
8srael <kouassiisrael36@gmail.com>
2023-08-05 19:29:48 +0000

    Task 0, creating an interface for a student🆗

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,39 @@
+// Define an interface for Student
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+// Creating two students
+
+const firstStudent: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+  location: 'New York'
+}
+
+const secondStudent: Student = {
+  firstName: 'Martin',
+  lastName: 'King',
+  age: 23,
+  location: 'Paris'
+}
+
+// Creating an array of students
+const studentsList: Array<Student> = [firstStudent, secondStudent];
+
+const table = document.querySelector('#students-table');
+
+if (table instanceof HTMLTableElement) {
+  studentsList.forEach((student) => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell(0);
+    const locationCell = row.insertCell(1);
+    firstNameCell.innerHTML = student.firstName;
+    locationCell.innerHTML = student.location;
+  });
+}
+
