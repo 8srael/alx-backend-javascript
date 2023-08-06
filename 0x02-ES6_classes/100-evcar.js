bc3aafa3ab0e69e94d3aa008ece805cb70ea8148
8srael <kouassiisrael36@gmail.com>
2023-08-06 19:39:26 +0000

    Last task completed✅

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,10 +6,6 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return this;
-  }
-
   cloneCar() {
     const Species = this.constructor[Symbol.species];
 
