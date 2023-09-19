049d35cd1c93288eef222cceeac85dc08beab191
8srael <kouassiisrael36@gmail.com>
2023-09-19 22:09:00 +0000

    Task 5. more complex http server with http module✅

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,60 @@
+/**
+ * Task 5. more complex HTTP server using Node's HTTP module
+ */
+
+const http = require('http');
+const fs = require('fs');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+
+    const dataLines = data.trim().split('\n');
+    const csStudents = dataLines
+      .filter((line) => line.includes('CS'))
+      .map((line) => line.substring(0, line.indexOf(',')));
+    const sweStudents = dataLines
+      .filter((line) => line.includes('SWE'))
+      .map((line) => line.substring(0, line.indexOf(',')));
+
+    resolve([csStudents, sweStudents]);
+  });
+});
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((data) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students
+Number of students: ${data[0].length + data[1].length}
+Number of students in CS: ${data[0].length}. List: ${data[0].join(', ')}
+Number of students in SWE: ${data[1].length}. List: ${data[1].join(', ')}`);
+      })
+      .catch((error) => {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('404 - Not found');
+  }
+});
+
+app.listen(port, hostname, () => {
+  process.stdout.write(`Server running at http://${hostname}:${port}\n`);
+});
+
+module.exports = app;
