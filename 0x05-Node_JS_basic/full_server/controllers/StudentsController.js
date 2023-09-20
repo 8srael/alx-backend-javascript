ab58d0800fbbc5d3c2e6e35304d8e02a210e3188
8srael <kouassiisrael36@gmail.com>
2023-09-20 18:47:41 +0000

    controllers🧹

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -8,38 +8,35 @@ const dbPath = process.argv.length > 2 ? process.argv[2] : '';
 
 class StudentsController {
   static getAllStudents(request, response) {
-    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     if (!dbPath) {
       response.status(500).send('Cannot load the database');
     }
     readDatabase(dbPath)
       .then((data) => {
-        response.end(`This is the list of our students
+        response.status(200).end(`This is the list of our students
 Number of students in ${Object.keys(data)[0]}: ${data.CS.length}. List: ${data.CS.join(', ')}
 Number of students in ${Object.keys(data)[1]}: ${data.SWE.length}. List: ${data.SWE.join(', ')}`);
       })
       .catch(() => {
-        response.statusCode = 500;
-        response.end('Cannot load the database');
+        response.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(request, response) {
-    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     readDatabase(dbPath)
       .then((data) => {
         const { major } = request.params;
         if (Object.keys(data).includes(major)) {
-          response.send(`List: ${data[major].join(', ')}`);
+          response.status(200).send(`List: ${data[major].join(', ')}`);
         } else {
           response.status(500).send('Major parameter must be CS or SWE');
         }
       })
       .catch((error) => {
         response.statusCode = 500;
-        response.end(error.message);
+        response.end('Cannot load the database');
       });
   }
 }
