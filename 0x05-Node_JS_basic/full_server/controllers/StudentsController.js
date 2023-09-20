9111e86f7cb43df85922dde73e0a1d9a45f0df37
8srael <kouassiisrael36@gmail.com>
2023-09-20 18:28:03 +0000

    Organize a comple HTTP server using Express🆗

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,43 @@
+import readDatabase from '../utils';
+
+/**
+ * StudentsController class for managing students
+ */
+
+const dbPath = process.argv.length > 2 ? process.argv[2] : '';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    readDatabase(dbPath)
+      .then((data) => {
+        response.end(`This is the list of our students
+Number of students in ${Object.keys(data)[0]}: ${data.CS.length}. List: ${data.CS.join(', ')}
+Number of students in ${Object.keys(data)[1]}: ${data.SWE.length}. List: ${data.SWE.join(', ')}\n`);
+      })
+      .catch((error) => {
+        response.end(error.message);
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    readDatabase(dbPath)
+      .then((data) => {
+        const { major } = request.params;
+        if (Object.keys(data).includes(major)) {
+          response.send(`List: ${data[major].join(', ')}\n`);
+        } else {
+          response.status(500).send('Major parameter must be CS or SWE\n');
+        }
+      })
+      .catch((error) => {
+        response.statusCode = 500;
+        response.end(error.message);
+      });
+  }
+}
+
+export default StudentsController;
