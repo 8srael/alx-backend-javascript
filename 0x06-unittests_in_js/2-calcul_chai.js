dc20027b997705cadfc6afba57eac5230fec4b7f
8srael <kouassiisrael36@gmail.com>
2023-09-23 01:25:19 +0000

    Task 2. lint passed✅

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -3,21 +3,20 @@
  */
 
 const calculateNumber = (type, a, b) => {
-    const aRounded = Math.round(a);
-    const bRounded = Math.round(b);
-  
-    if (type === 'SUM') {
-      return aRounded + bRounded;
-    } if (type === 'SUBTRACT') {
-      return aRounded - bRounded;
-    } if (type === 'DIVIDE') {
-      if (bRounded === 0) {
-        return 'Error';
-      }
-      return aRounded / bRounded;
+  const aRounded = Math.round(a);
+  const bRounded = Math.round(b);
+
+  if (type === 'SUM') {
+    return aRounded + bRounded;
+  } if (type === 'SUBTRACT') {
+    return aRounded - bRounded;
+  } if (type === 'DIVIDE') {
+    if (bRounded === 0) {
+      return 'Error';
     }
-    throw new TypeError('Type must be SUM, SUBTRACT or DIVIDE');
-  };
-  
-  module.exports = calculateNumber;
-  
+    return aRounded / bRounded;
+  }
+  throw new TypeError('Type must be SUM, SUBTRACT or DIVIDE');
+};
+
+module.exports = calculateNumber;
