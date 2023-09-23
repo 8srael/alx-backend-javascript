e4532519869d04658c8d326b864c5b4b6aca32d7
8srael <kouassiisrael36@gmail.com>
2023-09-23 01:18:31 +0000

    Task 2. basic test with chai🆗

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,23 @@
+/**
+ * Task 2: Basic test using Chai assertion library
+ */
+
+const calculateNumber = (type, a, b) => {
+    const aRounded = Math.round(a);
+    const bRounded = Math.round(b);
+  
+    if (type === 'SUM') {
+      return aRounded + bRounded;
+    } if (type === 'SUBTRACT') {
+      return aRounded - bRounded;
+    } if (type === 'DIVIDE') {
+      if (bRounded === 0) {
+        return 'Error';
+      }
+      return aRounded / bRounded;
+    }
+    throw new TypeError('Type must be SUM, SUBTRACT or DIVIDE');
+  };
+  
+  module.exports = calculateNumber;
+  
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,50 @@
+/**
+ * Test file of 2-calcul_chai module
+ */
+
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('Test for type=="SUM"', () => {
+    it('should return 4 when adding 1 and 3 rounded after rounding both', () => {
+      expect(calculateNumber('SUM', 1.0, 3.0)).to.equal(4);
+    });
+
+    it('should return 5 when adding 1 and 3.7 rounded after rounding both', () => {
+      expect(calculateNumber('SUM', 1.0, 3.7)).to.equal(5);
+    });
+
+    it('should return 7 when adding 3.499999 and 4.499999 after rounding both', () => {
+      expect(calculateNumber('SUM', 3.499999, 4.499999)).to.equal(7);
+    });
+  });
+  
+  describe('Test for type=="SUBTRACT"', () => {
+    it('should return 4 when subtracting 1 and 3', () => {
+      expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+    });
+    
+    it('should return 5 when subtracting 1 and 3.7 rounded after rounding both', () => {
+      expect(calculateNumber('SUBTRACT', 1.0, 3.7)).to.equal(-3);
+    });
+    
+    it('should return 5 when subtracting 1.2 and 3.7 after rounding both', () => {
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    });
+  });
+
+  describe('Test for type=="DIVIDE"', () => {
+    it('should return 0.2 when dividing 1.4 and 4.5 after rounding both', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+    
+    it('should return 2 when dividing 10 and 5 after rounding both', () => {
+      expect(calculateNumber('DIVIDE', 10, 5)).to.equal(2);
+    });
+    
+    it('should return Error when dividing a number by 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
